4f7e337a3a8d5cbefbde4a22064242f838087a5a
Thierry R <lavitrine.agency@gmail.com>
2026-05-12 10:41:50 +0200

    Disable debug mode in app.run

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def download_prompt():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
